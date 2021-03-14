35871f19aa4463076fa5312c4a90d28f26f67d42
Vivien Baguio <baguiov@posteo.net>
2021-03-14 15:06:49 +0100

    isdhuiahidhad

--- assets/script/deploy.sh
+++ assets/script/deploy.sh
@@ -15,7 +15,7 @@ echo "upload them with sshpass"
 echo $DEPLOY_USER
 echo $DEPLOY_HOST
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e scp -o stricthostkeychecking=no package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 
 # run the deploy script on the server
 echo "run the deploy script on the server"
